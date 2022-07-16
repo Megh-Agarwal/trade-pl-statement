77a440dba6879744811516c4d370a72975c5c79d
Megh Agarwal <megh2004pill@gmail.com>
2022-07-16 19:01:29 +0530

    Added notification check for accessibility and fixed other bugs

--- public/js/custom/add.js
+++ public/js/custom/add.js
@@ -2,6 +2,27 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function setInputDate(_id){
+    var _dat = document.querySelector(_id);
+    var hoy = new Date(),
+        d = hoy.getDate(),
+        m = hoy.getMonth()+1, 
+        y = hoy.getFullYear(),
+        data;
+
+    if(d < 10){
+        d = "0"+d;
+    };
+    if(m < 10){
+        m = "0"+m;
+    };
+
+    data = y+"-"+m+"-"+d;
+    _dat.value = data;
+};
+
+setInputDate("#tradeDate");
+
 var coinsList = [];
 var coinsSymbols = [];
 
